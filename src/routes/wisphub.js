f0fd5f4f31f9ff18b0b08bd5a38e02b455b803de
Jupagolab <jupagolab@gmail.com>
2026-03-14 12:14:18 -0400

    Editar ticket

--- src/routes/wisphub.js
+++ src/routes/wisphub.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { crearTicket } from "../controllers/wisphub.js";
+import { crearTicket, editarTicket } from "../controllers/wisphub.js";
 
 import multer from 'multer'
 import { verifyClientToken } from "../middleware/auth.js";
@@ -8,5 +8,6 @@ const router = Router();
 const upload = multer();
 
 router.post("/tickets", verifyClientToken, upload.none(), crearTicket)
+router.put("/tickets", verifyClientToken, upload.none(), editarTicket)
 
 export default router;
